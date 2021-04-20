d1ada0a19e1b53a56ccdc5f20b10950ee797ae28
AmitM0287 <amitmanna0287@gmail.com>
2021-04-20 13:54:10 +0530

    [Amit] Added: urls.py file updated

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -1,8 +1,14 @@
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from django.urls import path
+
 from . import views
 
 
 # Urls to handle specific request
 urlpatterns = [
-    path('user/notes/save/', views.SaveNotes.as_view()),
+    path('notes/', views.NotesList.as_view()),
+    path('notes/<int:pk>/', views.NotesDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
