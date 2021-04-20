3c29d73787099ead8d26b5dbdbe8d2e91fa5d8bb
AmitM0287 <amitmanna0287@gmail.com>
2021-04-20 13:51:08 +0530

    [Amit] Added: urls.py file updated

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 # Urls to handle specific request
 urlpatterns = [
     path('user/login/', views.UserLogin.as_view()),
-    path('user/register/', views.UserRegistration.as_view()),
+    path('user/register/', views.UserRegister.as_view()),
 ]
