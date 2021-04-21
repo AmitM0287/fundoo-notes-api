46a56fab4064206b12e40b2a728574a5eda4ff43
AmitM0287 <amitmanna0287@gmail.com>
2021-04-21 15:30:25 +0530

    [Amit] Added: urls.py file

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,9 +1,15 @@
+from rest_framework.urlpatterns import format_suffix_patterns 
 from django.urls import path, include
 from . import views
 
 
 # Urls to handle specific request
 urlpatterns = [
-    path('user/login/', views.UserLogin.as_view()),
-    path('user/register/', views.UserRegister.as_view()),
+    path('login/', views.UserLogin.as_view()),
+    path('register/', views.UserRegister.as_view()),
+    path('update/<int:id>/', views.UserUpdate.as_view()),
+    path('reset/password/<int:id>/', views.ResetPassword.as_view()),
+    path('delete/<int:id>/', views.UserDelete.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
