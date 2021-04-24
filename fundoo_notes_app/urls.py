021e934e323e8e945de6b111625a398891d4f3f5
AmitM0287 <amitmanna0287@gmail.com>
2021-04-24 22:56:40 +0530

    [Amit] Added: urls configuration

--- fundoo_notes_app/urls.py
+++ fundoo_notes_app/urls.py
@@ -1,23 +1,11 @@
-"""fundoo_notes_app URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib import admin
+
+
+# URL Configuration for fundoo_notes_app
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('auth_app.urls')),
-    path('notes/', include('notes_app.urls')),
+    path('auth/', include('auth_app.urls', namespace='auth_app')),
+    path('notes/', include('notes_app.urls', namespace='notes_app')),
 ]
