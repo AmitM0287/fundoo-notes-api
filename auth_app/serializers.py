767669d1138abd6038596b41e8cd7f6bb5da5412
AmitM0287 <amitmanna0287@gmail.com>
2021-04-24 22:50:56 +0530

    [Amit] Added: user serializers

--- auth_app/serializers.py
+++ auth_app/serializers.py
@@ -1,29 +1,21 @@
 from rest_framework import serializers
+
 from django.contrib.auth.models import User
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    """
-        User Register Serializer
-    """
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'email', 'username', 'password']
 
 
 class UserLoginSerializer(serializers.ModelSerializer):
-    """
-        User Login Serializer
-    """
     class Meta:
         model = User
         fields = ['username', 'password']
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
-    """
-        User Update Serializer
-    """
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'username']
+        fields = ['id', 'username']
