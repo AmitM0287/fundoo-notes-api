83213908b75da224126b0a890829526f0bc58da8
AmitM0287 <amitmanna0287@gmail.com>
2021-04-24 22:58:50 +0530

    [Amit] Added: notes model created

--- notes_app/models.py
+++ notes_app/models.py
@@ -1,8 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
-# Create Notes model
+# Notes model
 class Notes(models.Model):
     title = models.CharField(max_length=400)
     notes = models.CharField(max_length=1200)
