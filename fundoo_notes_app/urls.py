86b2ccf8715011dcfabe4e0536ede98e6432ea16
AmitM0287 <amitmanna0287@gmail.com>
2021-04-27 16:03:21 +0530

    [Amit] Added: urls updated

--- fundoo_notes_app/urls.py
+++ fundoo_notes_app/urls.py
@@ -6,6 +6,6 @@ from django.contrib import admin
 # URL Configuration for fundoo_notes_app
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('auth_app.urls', namespace='auth_app')),
-    path('notes/', include('notes_app.urls', namespace='notes_app')),
+    path('user/', include('auth_app.urls', namespace='auth_app')),
+    path('user/notes/', include('notes_app.urls', namespace='notes_app')),
 ]
