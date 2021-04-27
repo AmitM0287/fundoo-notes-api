10c0df07f87c54c7e8b5aa83a5d6a4dfbd9d5303
AmitM0287 <amitmanna0287@gmail.com>
2021-04-27 17:30:15 +0530

    [Amit] Added: serializers.py file updated

--- notes_app/serializers.py
+++ notes_app/serializers.py
@@ -3,15 +3,16 @@ from rest_framework import serializers
 from notes_app.models import Notes
 
 
-# Notes Serializer
 class NotesSerializer(serializers.ModelSerializer):
+    """
+        Notes Serializer
+    """
     class Meta:
         model = Notes
-        fields = ['id', 'title', 'notes', 'user_id']
+        fields = ['title', 'notes', 'user_id']
 
 
-# Notes Update Serializer
 class NotesUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notes
-        fields = ['id', 'title', 'notes']
+        fields = ['title', 'notes']
