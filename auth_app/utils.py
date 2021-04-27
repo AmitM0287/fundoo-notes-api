3d3c136a7a31206f4095b01e34c2f1a7f61818b7
AmitM0287 <amitmanna0287@gmail.com>
2021-04-27 15:45:52 +0530

    [Amit] Added: get object by id or username method

--- auth_app/utils.py
+++ auth_app/utils.py
@@ -1,16 +1,33 @@
 from django.contrib.auth.models import User
 
+from logging_config.logger import get_logger
 
-def get_user_instance(id):
-        """
-            This method is used to return user instance according to user id.
-            :param id: It's accept user id as parameter.
-            :return: It's return user instance or raise an exception.
-        """
-        try:
-            # Return user instance
-            return User.objects.get(id=id)
-        except User.DoesNotExist as e:
-            # Raise exception user does not exist.
-            logger.exception(e)
-            raise User.DoesNotExist
+
+# Configure logger
+logger = get_logger()
+
+
+def get_object_by_id(id):
+    """
+        This method is used to get object according to that id.
+        :param id: It's accept id as parameter.
+        :return: It's return that object.
+    """
+    try:
+        return User.objects.get(id=id)
+    except User.DoesNotExist as e:
+        logger.exception(e)
+        raise User.DoesNotExist
+
+
+def get_object_by_username(username):
+    """
+        This method is used to get object according to that username.
+        :param id: It's accept username as parameter.
+        :return: It's return that object.
+    """
+    try:
+        return User.objects.get(username=username)
+    except User.DoesNotExist as e:
+        logger.exception(e)
+        raise User.DoesNotExist
