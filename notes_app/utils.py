1520841008251ba871430717f897ea3262702736
AmitM0287 <amitmanna0287@gmail.com>
2021-04-27 17:31:19 +0530

    [Amit] Added: get notes by id or user_id

--- notes_app/utils.py
+++ notes_app/utils.py
@@ -1,34 +1,39 @@
 from notes_app.models import Notes
+from logging_config.logger import get_logger
 
 
-def get_instance_user_id(user_id):
-        """
-            This method is used to get notes instance according to user id.
-            :param user_id: It's accept user id as parameter.
-            :return: It's return notes instances or raise an exception.
-        """
-        try:
-            # Return notes instances
-            notes = Notes.objects.filter(user_id=user_id)
-            if not notes:
-                raise Notes.DoesNotExist
-            return list(notes)
-        except Notes.DoesNotExist as e:
-            # Raise exception user does not exist.
-            logger.exception(e)
+# Configure logger
+logger = get_logger()
+
+
+def get_notes_by_id(id):
+    """
+        This method is used to get notes according to that id.
+        :param id: It's accept id as parameter.
+        :return: It's return that object.
+    """
+    try:
+        # Return notes object
+        return Notes.objects.get(id=id)
+    except Notes.DoesNotExist as e:
+        # Raise exception notes does not exist.
+        logger.exception(e)
+        raise Notes.DoesNotExist
+
+
+def get_notes_by_user_id(user_id):
+    """
+        This method is used to get notes according to that user_id.
+        :param id: It's accept user id as parameter.
+        :return: It's return that object.
+    """
+    try:
+        # Return notes object
+        notes = Notes.objects.filter(user_id=user_id)
+        if not notes:
             raise Notes.DoesNotExist
-
-
-def get_instance_notes_id(notes_id):
-        """
-            This method is used to get notes instance according to notes id.
-            :param user_id: It's accept notes id as parameter.
-            :return: It's return notes instance or raise an exception.
-        """
-        try:
-            # Return notes instance
-            return Notes.objects.get(id=notes_id)
-        except Notes.DoesNotExist as e:
-            # Raise exception notes does not exist.
-            logger.exception(e)
-            raise Notes.DoesNotExist
+        return list(notes)
+    except Notes.DoesNotExist as e:
+        # Raise exception notes does not exist.
+        logger.exception(e)
+        raise Notes.DoesNotExist
