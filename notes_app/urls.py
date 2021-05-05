d50c5d5c580c571f4e8aebde3289853d113e2612
AmitM0287 <amitmanna0287@gmail.com>
2021-05-05 22:06:11 +0530

    [Amit] Added: new urls added

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -7,4 +7,5 @@ from notes_app import views
 app_name='notes_app'
 urlpatterns = [
     path('', views.NotesCRUD.as_view(), name='notes_crud'),
+    path('labels/', views.LabelsCRUD.as_view(), name='labels_crud'),
 ]
