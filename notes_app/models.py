63ca5fd1e7b712fb5c80d18b3a8d16907713fa39
AmitM0287 <amitmanna0287@gmail.com>
2021-05-05 22:04:28 +0530

    [Amit] Added: models.py file updated

--- notes_app/models.py
+++ notes_app/models.py
@@ -2,8 +2,24 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# Notes model
+class Labels(models.Model):
+    """
+     Labels Model
+    """
+    name = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
 class Notes(models.Model):
+    """
+        Notes Model
+    """
     title = models.CharField(max_length=400)
-    notes = models.CharField(max_length=1200)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=1200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    collaborator = models.ManyToManyField(User, related_name='collaborator')
+    label = models.ManyToManyField(Labels)
