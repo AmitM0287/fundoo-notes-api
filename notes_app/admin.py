6b8b1cf6ae0cbb2fa32e868a1a8f30eb3c548b35
AmitM0287 <amitmanna0287@gmail.com>
2021-05-05 22:03:12 +0530

    [Amit] Added: unregister models

--- notes_app/admin.py
+++ notes_app/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
-from notes_app.models import Notes
-
-
-# Register Notes model 
-@admin.register(Notes)
-class NotesAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'notes', 'user_id']
+# Register your models here.
