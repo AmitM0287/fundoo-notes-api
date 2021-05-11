44d044630d3d74b4db191c34038e7feabd90c371
AmitM0287 <amitmanna0287@gmail.com>
2021-05-11 10:39:26 +0530

    [Amit] Added: admin.py file updated

--- notes_app/admin.py
+++ notes_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from notes_app.models import Notes, Labels
 
-# Register your models here.
+
+# Register Notes model
+@admin.register(Notes)
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'user_id']
