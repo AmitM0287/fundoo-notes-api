9f61127c7b7f380df3df1ba380c597739db91868
AmitM0287 <amitmanna0287@gmail.com>
2021-05-11 10:40:18 +0530

    [Amit] Added: serializers.py file updated

--- notes_app/serializers.py
+++ notes_app/serializers.py
@@ -5,19 +5,22 @@ from notes_app.models import Notes, Labels
 
 class NotesSerializer(serializers.ModelSerializer):
     """
-        Notes Serializer
+        Notes Serializer : title, description
     """
     title = serializers.CharField(min_length=4, max_length=400, required=True)
-    notes = serializers.CharField(min_length=4, max_length=1200, required=True)
+    description = serializers.CharField(min_length=4, max_length=1200, required=True)
+
     class Meta:
         model = Notes
-        fields = ['title', 'notes']
+        fields = ['title', 'description']
 
 
 class LabelsSerializer(serializers.ModelSerializer):
     """
-        Labels Serializer
+        Labels Serializer : name
     """
+    name = serializers.CharField(min_length=2, max_length=200, required=True)
+    
     class Meta:
         model = Labels
         fields = ['name']
