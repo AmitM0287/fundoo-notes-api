22807e9495927c8f95a42d7ecf8c5e6a236b936e
AmitM0287 <amitmanna0287@gmail.com>
2021-05-22 08:44:37 +0530

    [Amit] Added: urls are updated

--- fundoo_notes_app/urls.py
+++ fundoo_notes_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 # URL Configuration for fundoo_notes_app
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='social_app/index.html')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('user/', include('auth_app.urls', namespace='auth_app')),
     path('user/notes/', include('notes_app.urls', namespace='notes_app')),
 ]
