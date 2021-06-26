4d20422b81f0311baf0d8e6256c39c1d901e1d12
AmitM0287 <amitmanna0287@gmail.com>
2021-06-26 08:21:50 +0530

    [Amit] Added: views.py file updated

--- notes_app/views.py
+++ notes_app/views.py
@@ -14,6 +14,9 @@ from notes_app.models import Notes, Labels
 from notes_app.utils import get_notes_by_id, get_notes_by_user_id, varify_token, get_labels_by_id, get_labels_by_user_id
 from notes_app.elastic_search import ElasticSearch
 
+import xlsxwriter
+import pandas
+
 
 # Logger configuration
 logger = get_logger()
@@ -36,13 +39,13 @@ class NotesAPIView(APIView):
             # Notes serializer
             serializer = NotesSerializer(notes, many=True)
             # Getting notes from elastic search
-            es = ElasticSearch()
-            es_data = es.get_data(user_id=user_id)
-            data = []
-            for ite in range(len(es_data)):
-                data.append(es_data[ite]['_source'])
+            # es = ElasticSearch()
+            # es_data = es.get_data(user_id=user_id)
+            # data = []
+            # for ite in range(len(es_data)):
+            #     data.append(es_data[ite]['_source'])
             # Return notes instances
-            return Response({'success': True, 'message': 'Getting notes successfully!', 'data': {'notes_list': data}}, status=status.HTTP_200_OK)
+            return Response({'success': True, 'message': 'Getting notes successfully!', 'data': {'notes_list': serializer.data}}, status=status.HTTP_200_OK)
         except Notes.DoesNotExist as e:
             logger.exception(e)
             return Response({'success': False, 'message': 'You do not have any notes!'}, status=status.HTTP_400_BAD_REQUEST)
@@ -67,8 +70,8 @@ class NotesAPIView(APIView):
             data = serializer.data
             data.update({'id': notes.id})
             # Save data into elastic search
-            es = ElasticSearch()
-            es.save_data(title=serializer.data.get('title'), description=serializer.data.get('description'), user_id=user_id)
+            # es = ElasticSearch()
+            # es.save_data(title=serializer.data.get('title'), description=serializer.data.get('description'), user_id=user_id)
             #Success response
             return Response({'success': True, 'message': 'Notes created successfully!', 'data': {'notes_list': data}}, status=status.HTTP_200_OK)
         except ValidationError as e:
@@ -92,8 +95,8 @@ class NotesAPIView(APIView):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             # Update data in elastic search
-            es = ElasticSearch()
-            es.update_data(doc_id=request.data.get('id'), es_data={'doc': { 'title': request.data.get('title'), 'description': request.data.get('description')}})
+            # es = ElasticSearch()
+            # es.update_data(doc_id=request.data.get('id'), es_data={'doc': { 'title': request.data.get('title'), 'description': request.data.get('description')}})
             # Notes updated successfully
             return Response({'success': True, 'message': 'Notes updated successfully!', 'data': {'notes_list': serializer.data}}, status=status.HTTP_202_ACCEPTED)
         except Notes.DoesNotExist:
@@ -117,9 +120,9 @@ class NotesAPIView(APIView):
             notes = get_notes_by_id(request.data.get('id'))
             # Delete notes instance
             notes.delete()
-            # Delete notes from elastic search
-            es = ElasticSearch()
-            es.delete_data(doc_id=request.data.get('id'))
+            # # Delete notes from elastic search
+            # es = ElasticSearch()
+            # es.delete_data(doc_id=request.data.get('id'))
             # Notes deleted successfully
             return Response({'success': True, 'message': 'Notes deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
         except Notes.DoesNotExist:
@@ -128,6 +131,56 @@ class NotesAPIView(APIView):
         except Exception as e:
             logger.exception(e)
             return Response({'success': False, 'message': 'Oops! Something went wrong!'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArchiveNotesAPIView(APIView):
+    """
+        ArchiveNotesAPIView: Archive & UnArchive notes.
+    """
+    def post(self, request):
+        """
+            This method is used for archive & unarchive notes.
+            :param request: It's accept note_id as parameter.
+            :return: It's return response that notes succcessfully archive or unarchive.
+        """
+        try:
+            note_id = request.data.get('note_id')
+            note = get_notes_by_id(id=note_id)
+            if note.isArchive == False:
+                note.isArchive = True
+            else:
+                note.isArchive = False
+            # Save changes
+            note.save()
+            return Response({'success': True, 'message': 'Notes isArchive successful!'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.exception(e)
+            return Response({'success': False, 'message': 'Oops! Something went wrong! Please try again...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TrashNotesAPIView(APIView):
+    """
+        TrashNotesAPIView: Trash notes $ Restore notes.
+    """
+    def post(self, request):
+        """
+            This method is used for trash notes & restore notes.
+            :param request: It's accept note_id as parameter.
+            :return: It's return response that notes succcessfully trash or restore.
+        """
+        try:
+            note_id = request.data.get('note_id')
+            note = get_notes_by_id(id=note_id)
+            if note.isTrash == False:
+                note.isTrash = True
+            else:
+                note.isTrash = False
+            # Save changes
+            note.save()
+            return Response({'success': True, 'message': 'Notes isTrash successful!'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.exception(e)
+            return Response({'success': False, 'message': 'Oops! Something went wrong! Please try again...'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LabelsAPIView(APIView):
@@ -217,6 +270,37 @@ class LabelsAPIView(APIView):
         except Labels.DoesNotExist:
             # Labels does not exist
             return Response({'success': False, 'message': 'Labels does not exist!'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            logger.exception(e)
+            return Response({'success': False, 'message': 'Oops! Something went wrong!'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ExcelSheetAPIView(APIView):
+    # def post(self, request):
+    #     try:
+    #         # Create a workbook and add a worksheet.
+    #         workbook = xlsxwriter.Workbook('/home/amitm/Desktop/notes.xlsx')
+    #         worksheet = workbook.add_worksheet('notes')
+    #         data = request.data
+    #         keys = list(data.keys())
+    #         values = list(data.values())
+    #         # Write data into excel sheet
+    #         for ite in range(len(keys)):
+    #             worksheet.write(0, ite, keys[ite])
+    #             worksheet.write(1, ite, values[ite])
+    #         workbook.close()
+    #         return Response({'success': True, 'message': 'Data inserted successfully into excel sheet!'}, status=status.HTTP_200_OK)
+    #     except Exception as e:
+    #         logger.exception(e)
+    #         return Response({'success': False, 'message': 'Oops! Something went wrong!'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        try:
+            # data = pandas.read_excel('/home/amitm/Desktop/notes.xlsx')
+            # print(request.form['file'])
+            data = pandas.read_excel(request.data['file'])
+            print(data.to_dict('dict'))
+            return Response({'success': True, 'message': 'Get data successfully from excel sheet!'}, status=status.HTTP_200_OK)
         except Exception as e:
             logger.exception(e)
             return Response({'success': False, 'message': 'Oops! Something went wrong!'}, status=status.HTTP_400_BAD_REQUEST)
