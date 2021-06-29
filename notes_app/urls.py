62dd407f7b7f608c9a1b4f55bda5461ea4cb0c7d
AmitM0287 <amitmanna0287@gmail.com>
2021-06-29 19:09:43 +0530

    [Amit] Added: delete notes url added

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.NotesAPIView.as_view(), name='notes_crud'),
     path('is-archive/', views.ArchiveNotesAPIView.as_view(), name='is-archive'),
     path('is-trash/', views.TrashNotesAPIView.as_view(), name='is-trah'),
+    path('<id>/', views.DeleteNoteAPIView.as_view(), name='notes_delete'),
     path('labels/', views.LabelsAPIView.as_view(), name='labels_crud'),
     path('data/excel-sheet/', views.ExcelSheetAPIView.as_view(), name='excel_sheet'),
 ]
