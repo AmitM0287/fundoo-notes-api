cf66550e06e0200448385ac4850b2a586b63e454
AmitM0287 <amitmanna0287@gmail.com>
2021-07-07 15:05:14 +0530

    [Amit] Added: file updated

--- auth_app/send.py
+++ auth_app/send.py
@@ -1,17 +1,17 @@
-import pika
+# import pika
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+# connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+# channel = connection.channel()
 
-channel.queue_declare(queue='send_mail')
+# channel.queue_declare(queue='send_mail')
 
 
-def send_data_to_queue(data):
-    """
-        This method is used to send data to queue.
-        :param data: It's accept data in dictionary format.
-        :return: None
-    """
-    channel.basic_publish(exchange='', routing_key='send_mail', body=data)
-    print('[x] Data sended to queue.')
+# def send_data_to_queue(data):
+#     """
+#         This method is used to send data to queue.
+#         :param data: It's accept data in dictionary format.
+#         :return: None
+#     """
+#     channel.basic_publish(exchange='', routing_key='send_mail', body=data)
+#     print('[x] Data sended to queue.')
